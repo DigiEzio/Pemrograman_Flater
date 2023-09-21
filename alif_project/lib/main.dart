454a3a3b09409df5c79579a28b00a84acaa0c657
DigiEzio <128774992+DigiEzio@users.noreply.github.com>
2023-09-21 16:11:11 +0700

    hello world

---
+++ alif_project/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget{
+
+  Widget build(BuildContext context){ 
+
+    return MaterialApp(home:Center(child:Text('Alif Nur Samudra NoBP 2201082021'),),);
+  }
+
+}
